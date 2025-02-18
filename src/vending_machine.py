5b97bdb57a5014c2264d26cc5ebc58e3e840061a
William He <wjhe2@wisc.edu>
2025-02-17 19:30:50 -0600

    Added initial implementation of VendingMachine as detailed in #15 in vending_machine.py

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -17,17 +17,17 @@ class VendingMachine:
 
     Methods
     -------
-    def list_options() -> str
+    def list_options(self) -> str
         Returns a string representation of the inventory of the vending machine
-    def start_transaction() -> None
+    def start_transaction(self) -> None
         Sets "transaction_in_progress" to true
         Calls Stripe API to get stripe_payment_token for api calls
-    def buy_item() -> None
+    def buy_item(self, slot_name) -> None
         Only callable if transaction_in_progress is True
         Dispense item selected
         Update stock information and database (might happen in inventory_manager implementation)
         Add price of item to transaction_price
-    def end_transaction() -> None
+    def end_transaction(self) -> None
         Use Stripe API to charge user's payment method with transaction_price
         Clear transaction_price and stripe_payment_token
         Set transaction_in_progress to False
@@ -43,9 +43,32 @@ class VendingMachine:
 
     def list_options(self) -> str:
         return self.inv_man.get_stock_information()
-    
+
 
     def start_transaction(self) -> None:
         if(self.transaction_in_progress is True):
-            raise 
-        self.transaction_in_progress
+            raise ValueError("Transaction has already been started")
+        self.transaction_in_progress = True
+
+        # stripe API implementation to log user in and obtain API token
+        # self.stripe_payment_token = <API token>
+
+
+    def buy_item(self, slot_name: str) -> None:
+        if(not self.transaction_in_progress):
+            raise ValueError("buy_item() can only be called when transaction is in progress. "\
+                             "Call start_transaction() first")
+
+        purchase_price = self.inv_man.change_stock(slot_name, -1)
+        self.transaction_price += purchase_price
+
+
+    def end_transaction(self) -> None:
+        if(not self.transaction_in_progress):
+            raise ValueError("Transaction is not currently in progress, start a transaction first")
+
+        # Use stripe API to charge self.transaction_price with self.stripe_payment_token
+        self.transaction_price = 0
+        self.stripe_payment_token = None
+
+        self.transaction_in_progress = False
