7b3b4dad70b4d60904a527c27afd5918e0c47998
William He <wjhe2@wisc.edu>
2025-02-18 00:40:38 -0600

    Moved transaction and restocking state booleans into ENUM within inventory_manager class. Removed all vendor side functionality from vending machine class. The Mode enum is stored in inventory_manager_mode.py. VendingMachine class now takes an inventory_manager as an argument in the constructor, so CLI can access it from outside the vending machine class.

--- src/inventory_manager.py
+++ src/inventory_manager.py
@@ -1,4 +1,5 @@
-import item as item_module
+from inventory_manager_mode import Mode
+from item import Item
 
 
 class InventoryManager:
@@ -8,9 +9,15 @@ class InventoryManager:
     ----------
     items: Item[][]
         2d List of all items
+    mode
+        Operating mode of inventory manager, is either IDLE, TRANSACTION, or RESTOCKING
 
     Methods
     -------
+    def get_mode(self) -> Mode
+        Returns the operating mode of this inventory manager
+    def set_mode(self, new_mode) -> None
+        Sets the operating mode of this inventory manager
     def get_stock_information(self, show_empty_slots) -> str
         Return a string of all stock information
     def change_stock(self, slot_name, item_stock) -> float
@@ -38,6 +45,26 @@ class InventoryManager:
            height > self.MAXHEIGHT or width > self.MAXWIDTH):
             raise ValueError("Width and Height must be 0 < x < 11")
         self.items = [[None for i in range(width)] for j in range(height)]
+        self.mode = Mode.IDLE
+
+
+    def get_mode(self) -> Mode:
+        return self.mode
+
+
+    def set_mode(self, new_mode: Mode) -> None:
+        if(new_mode is Mode.IDLE and self.mode is Mode.IDLE):
+                raise ValueError("Cannot change mode from IDLE to IDLE")
+
+        if(new_mode is Mode.TRANSACTION and self.mode is not Mode.IDLE):
+            raise ValueError("Mode must be IDLE before changing to TRANSACTION, not" \
+                             + Mode.IDLE.name)
+
+        if(new_mode is Mode.RESTOCKING and self.mode is not Mode.IDLE):
+            raise ValueError("Mode must be IDLE before changing to RESTOCKING, not" \
+                             + Mode.IDLE.name)
+
+        self.mode = new_mode
 
 
     def get_stock_information(self, show_empty_slots: bool = False) -> str:
@@ -45,7 +72,7 @@ class InventoryManager:
 
         for r in range(len(self.items)):
             for c in range(len(self.items[0])):
-                item: item_module.Item = self.items[r][c]
+                item: Item = self.items[r][c]
                 if(item is None):
                     if(show_empty_slots):
                         out += f"{r}{c}: <EMPTY>\n"
@@ -62,7 +89,7 @@ class InventoryManager:
     def change_stock(self, slot_name: str, item_stock: int) -> float:
         itemrow, itemcol = self.__get_coordinates_from_slotname(slot_name)
 
-        item: item_module.Item = self.items[itemrow][itemcol]
+        item: Item = self.items[itemrow][itemcol]
         if(item is None): raise ValueError("No item at slot " + slot_name)
         item.adjust_stock(item_stock)
         if(item_stock < 0):
@@ -72,7 +99,7 @@ class InventoryManager:
 
     def add_item(self, slot_name: str, item_name: str, item_stock: int, item_cost: float) -> None:
         itemrow, itemcol = self.__get_coordinates_from_slotname(slot_name)
-        new_item: item_module.Item = item_module.Item(item_name, item_cost, item_stock)
+        new_item: Item = Item(item_name, item_cost, item_stock)
         self.items[itemrow][itemcol] = new_item
 
 
@@ -84,7 +111,7 @@ class InventoryManager:
     def set_cost(self, slot_name: str, new_cost: float) -> None:
         itemrow, itemcol = self.__get_coordinates_from_slotname(slot_name)
 
-        item: item_module.Item = self.items[itemrow][itemcol]
+        item: Item = self.items[itemrow][itemcol]
         if(item is None): raise ValueError("No item at slot " + slot_name)
 
         item.set_cost(new_cost)
