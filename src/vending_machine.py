4ae08abe26fbb8df6198171c4071489cdc08cf90
William He <wjhe2@wisc.edu>
2025-02-18 00:40:38 -0600

    Moved transaction and restocking state booleans into ENUM within inventory_manager class. Removed all vendor side functionality from vending machine class. The Mode enum is stored in inventory_manager_mode.py. VendingMachine class now takes an inventory_manager as an argument in the constructor, so CLI can access it from outside the vending machine class.

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -1,4 +1,5 @@
-import inventory_manager
+from inventory_manager import InventoryManager
+from inventory_manager_mode import Mode
 
 
 class VendingMachine:
@@ -6,84 +7,55 @@ class VendingMachine:
 
     Attributes
     ----------
-    inv_man: inventory_manager.InventoryManager
+    inv_man: InventoryManager
         Instance of InventoryManager class that manages vending machine inventory
     stripe_payment_token: str
         Token used to call Stripe API
-    transaction_in_progress: bool
-        Signifies whether a transaction is currently in progress
     transaction_price: float
         The total price of the current ongoing transaction
-    restocking_in_progress: bool
-        Signifies whether machine is currently being restocked
 
     Methods
     -------
     def list_options(self, show_empty_slots = False) -> str
         Returns a string representation of the inventory of the vending machine
-        Optionally show all slots with 0 stock
     def start_transaction(self) -> None
-        Only callable if transaction_in_progress is False
-        Sets "transaction_in_progress" to true
+        Only callable if mode of inv_man is IDLE
+        Sets mode of inv_man to TRANSACTION
         Calls Stripe API to get stripe_payment_token for api calls
     def buy_item(self, slot_name) -> None
-        Only callable if transaction_in_progress is True
+        Only callable if mode of inv_man is TRANSACTION
         Dispense item selected
         Update stock information and database (might happen in inventory_manager implementation)
         Add price of item to transaction_price
     def end_transaction(self) -> None
-        Only callable if transaction_in_progress is True
+        Only callable if mode of inv_man is TRANSACTION
         Use Stripe API to charge user's payment method with transaction_price
         Clear transaction_price and stripe_payment_token
-        Set transaction_in_progress to False
-    def start_restocking(self) -> None
-        Only callable if transaction_in_progress is False
-        Only callable if restocking_in_progress is False
-        Sets restocking_in_progress to True
-        Makes all restocking functions callable (restock slot)
-    def adjust_slot_stock(self, slot_name, adjustment_value) -> None
-        Only callable if restocking_in_progress is True
-        Used to add or remove items to a slot's stock
-    def clear_slot(self, slot_name) -> None
-        Only callable if restocking_in_progress is True
-        Used to remove an item from a slot
-    def add_item(self, slot_name, item_name, item_cost, item_stock) -> None
-        Only callable if restocking_in_progress is True
-        Add an item to an empty slot or override current item
-    def set_cost(self, slot_name, new_cost) -> None
-        Only callable if restocking_in_progress is True
-        Update the cost of an existing item to new_cost
-    def end_restocking(self) -> None
-        Only callable if restocking_in_progress is True
-        Sets restocking_in_progress back to False
+        Sets mode of inv_man to IDLE
 
     """
 
-    def __init__(self, rows: int, cols: int):
-        self.inv_man = inventory_manager.InventoryManager(rows, cols)
+    def __init__(self, inventory_manager: InventoryManager):
+        self.inv_man = inventory_manager
         self.stripe_payment_token: str = None
-        self.transaction_in_progress: bool = False
-        self.restocking_in_progress: bool = False
         self.transaction_price: float = 0
 
 
-    def list_options(self, show_empty_slots: bool = False) -> str:
-        return self.inv_man.get_stock_information(show_empty_slots=show_empty_slots)
+    def list_options(self) -> str:
+        return self.inv_man.get_stock_information()
 
 
     def start_transaction(self) -> None:
-        if(self.restocking_in_progress is True):
-            raise ValueError("Cannot start transaction while restocking in progress")
-        if(self.transaction_in_progress is True):
-            raise ValueError("Transaction has already been started")
-        self.transaction_in_progress = True
+        if(self.inv_man.get_mode() is not Mode.IDLE):
+            raise ValueError("Vending Machine must be IDLE to start transaction")
+        self.inv_man.set_mode(Mode.TRANSACTION)
 
         # stripe API implementation to log user in and obtain API token
         # self.stripe_payment_token = <API token>
 
 
     def buy_item(self, slot_name: str) -> None:
-        if(not self.transaction_in_progress):
+        if(self.inv_man.get_mode() is not Mode.TRANSACTION):
             raise ValueError("buy_item() can only be called when transaction is in progress. "\
                              "Call start_transaction() first")
 
@@ -92,59 +64,11 @@ class VendingMachine:
 
 
     def end_transaction(self) -> None:
-        if(not self.transaction_in_progress):
+        if(self.inv_man.get_mode() is not Mode.TRANSACTION):
             raise ValueError("Transaction is not currently in progress, start a transaction first")
 
         # Use stripe API to charge self.transaction_price with self.stripe_payment_token
         self.transaction_price = 0
         self.stripe_payment_token = None
 
-        self.transaction_in_progress = False
-
-
-    def start_restocking(self) -> None:
-        if(self.restocking_in_progress is True):
-            raise ValueError("Restocking has already been started")
-        if(self.transaction_in_progress is True):
-            raise ValueError("Cannot start restocking while transaction in progress")
-
-        self.restocking_in_progress = True
-
-
-    def adjust_slot_stock(self, slot_name: str, adjustment_value: int) -> None:
-        if(not self.restocking_in_progress):
-            raise ValueError("adjust_slot_stock() can only be called when restocking. "\
-                             "Call start_restocking() first")
-
-        self.inv_man.change_stock(slot_name, adjustment_value)
-
-
-    def clear_slot(self, slot_name: str) -> None:
-        if(not self.restocking_in_progress):
-            raise ValueError("clear_slot() can only be called when restocking. "\
-                             "Call start_restocking() first")
-
-        self.inv_man.clear_slot(slot_name)
-
-
-    def add_item(self, slot_name: str, item_name: str, item_cost: float, item_stock: int) -> None:
-        if(not self.restocking_in_progress):
-            raise ValueError("add_item() can only be called when restocking. "\
-                             "Call start_restocking() first")
-
-        self.inv_man.add_item(slot_name, item_name, item_stock, item_cost)
-
-
-    def set_cost(self, slot_name: str, new_cost: float) -> None:
-        if(not self.restocking_in_progress):
-            raise ValueError("set_cost() can only be called when restocking. "\
-                             "Call start_restocking() first")
-
-        self.inv_man.set_cost(slot_name, new_cost)
-
-
-    def end_restocking(self) -> None:
-        if(not self.restocking_in_progress):
-            raise ValueError("Restocking is not currently in progress, start restocking first")
-
-        self.restocking_in_progress = False
+        self.inv_man.set_mode(Mode.IDLE)
