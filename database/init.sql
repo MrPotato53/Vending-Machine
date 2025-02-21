7799c102cfd5a3446995a172819eefd6af56b352
William He <wjhe2@wisc.edu>
2025-02-21 04:35:13 -0600

    made item_name unique and made composite primary key in join table

--- database/init.sql
+++ database/init.sql
@@ -8,15 +8,16 @@ CREATE TABLE IF NOT EXISTS vending_machines (
 
 CREATE TABLE IF NOT EXISTS items (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    item_name VARCHAR(255) NOT NULL
+    item_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS inventory_join_table (
-    vending_machine_id INT NOT NULL PRIMARY KEY,
-    slot_name VARCHAR(5) NOT NULL PRIMARY KEY,
+    vending_machine_id INT NOT NULL,
+    slot_name VARCHAR(5) NOT NULL,
     item_id INT NOT NULL,
     price DECIMAL(10, 2) UNSIGNED NOT NULL,
     stock INT UNSIGNED NOT NULL,
+    PRIMARY KEY (vending_machine_id, slot_name),
     FOREIGN KEY (vending_machine_id) REFERENCES vending_machines(id) ON DELETE CASCADE,
     FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
 );
