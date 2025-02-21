ef4a56e8fa629790d8311c57f630fbad40f50a39
William He <wjhe2@wisc.edu>
2025-02-20 20:06:27 -0600

    created initial versions of compose and sql init file, have not confirmed functionality

---
+++ database/init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS vending_machines (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vending_machine_name VARCHAR(100),
+    row_count INT UNSIGNED NOT NULL,
+    column_count INT UNSIGNED NOT NULL,
+    -- vendor_id INT
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS inventory_join_table (
+    vending_machine_id INT NOT NULL PRIMARY KEY,
+    slot_name VARCHAR(5) NOT NULL PRIMARY KEY,
+    item_id INT NOT NULL,
+    price DECIMAL(10, 2) UNSIGNED NOT NULL,
+    stock INT UNSIGNED NOT NULL,
+    FOREIGN KEY (vending_machine_id) REFERENCES vending_machines(id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
