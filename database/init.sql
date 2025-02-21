59bb9c7436e723193118752a7a1aa60d331f8ef1
William He <wjhe2@wisc.edu>
2025-02-21 14:51:04 -0600

    inproved naming clarity, set autoincrement values for vending_machine and items tables

--- database/init.sql
+++ database/init.sql
@@ -1,17 +1,21 @@
 USE VendingMachineDB;
 
 CREATE TABLE IF NOT EXISTS vending_machines (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    vending_machine_name VARCHAR(100),
+    vm__id INT AUTO_INCREMENT PRIMARY KEY,
+    vm_name VARCHAR(100),
     row_count INT UNSIGNED NOT NULL,
-    column_count INT UNSIGNED NOT NULL
+    column_count INT UNSIGNED NOT NULL,
+    PRIMARY KEY(vending_machine_id)
     -- vendor_id INT
 );
+alter table vending_machines auto_increment = 1000001;
 
 CREATE TABLE IF NOT EXISTS items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    item_name VARCHAR(255) NOT NULL UNIQUE
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY(item_id)
 );
+alter table items auto_increment = 2000001;
 
 CREATE TABLE IF NOT EXISTS inventory_join_table (
     vending_machine_id INT NOT NULL,
@@ -20,6 +24,6 @@ CREATE TABLE IF NOT EXISTS inventory_join_table (
     price DECIMAL(10, 2) UNSIGNED NOT NULL,
     stock INT UNSIGNED NOT NULL,
     PRIMARY KEY (vending_machine_id, slot_name),
-    FOREIGN KEY (vending_machine_id) REFERENCES vending_machines(id) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+    FOREIGN KEY (vending_machine_id) REFERENCES vending_machines(vm_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
 );
