038f2baf14304939fa13cb4ae89003dafa542741
William He <wjhe2@wisc.edu>
2025-02-22 19:07:00 -0600

    Added db_manager.sh script to automate shutting down and wiping database info and restarting container. Fixed errors in init.sql.

--- database/init.sql
+++ database/init.sql
@@ -1,21 +1,19 @@
 USE VendingMachineDB;
 
 CREATE TABLE IF NOT EXISTS vending_machines (
-    vm__id INT AUTO_INCREMENT PRIMARY KEY,
+    vm_id INT AUTO_INCREMENT PRIMARY KEY,
     vm_name VARCHAR(100),
     row_count INT UNSIGNED NOT NULL,
-    column_count INT UNSIGNED NOT NULL,
-    PRIMARY KEY(vending_machine_id)
+    column_count INT UNSIGNED NOT NULL
     -- vendor_id INT
 );
-alter table vending_machines auto_increment = 1000001;
+ALTER TABLE vending_machines AUTO_INCREMENT = 1000001;
 
 CREATE TABLE IF NOT EXISTS items (
     item_id INT AUTO_INCREMENT PRIMARY KEY,
-    item_name VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY(item_id)
+    item_name VARCHAR(255) NOT NULL UNIQUE
 );
-alter table items auto_increment = 2000001;
+ALTER TABLE items AUTO_INCREMENT = 2000001;
 
 CREATE TABLE IF NOT EXISTS inventory_join_table (
     vending_machine_id INT NOT NULL,
