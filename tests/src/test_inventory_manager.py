497d4800095ae5cdcf866731c083abf4bbbd1c04
Drew Broch <abroch@wisc.edu>
2025-02-26 14:24:53 -0600

    Completed testing for Inventory manager

--- tests/src/test_inventory_manager.py
+++ tests/src/test_inventory_manager.py
@@ -51,3 +51,63 @@ def test_get_stock_information(inventory_manager: InventoryManager) -> None:
     inventory_manager.add_item("00", "Soda", 10, 1.5)
     stock_info = inventory_manager.get_stock_information()
     assert "00: Soda, Price: 1.5, Left in Stock: 10" in stock_info
+
+
+def test_change_stock(inventory_manager: InventoryManager) -> None:
+    """Tests changing stock and that illegal updates raise an error."""
+    inventory_manager.add_item("00", "Soda", 10, 1.5)
+    cost = inventory_manager.change_stock("00", -2)
+    assert cost == 3.0
+
+    with pytest.raises(err.EmptySlotError):
+        inventory_manager.change_stock("01", 5)
+
+
+def test_add_item(inventory_manager: InventoryManager) -> None:
+    """Tests that adding an item to the inventory works."""
+    inventory_manager.add_item("00", "Soda", 10, 1.5)
+    item = inventory_manager.get_item("00")
+    assert item.get_name() == "Soda"
+    assert item.get_cost() == 1.5
+    assert item.get_stock() == 10
+
+
+def test_clear_slot(inventory_manager: InventoryManager) -> None:
+    """Test clearing slot and that removing empty slot throws an error."""
+    inventory_manager.add_item("00", "Soda", 10, 1.5)
+    inventory_manager.clear_slot("00")
+    with pytest.raises(err.EmptySlotError):
+        inventory_manager.get_item("00")
+
+
+def test_set_cost(inventory_manager: InventoryManager) -> None:
+    """Test updating the cost to a slot and that setting a cost for an empty slot is an error."""
+    inventory_manager.add_item("00", "Soda", 10, 1.5)
+    inventory_manager.set_cost("00", 2.0)
+    item = inventory_manager.get_item("00")
+    assert item.get_cost() == 2.0
+
+    with pytest.raises(err.EmptySlotError):
+        inventory_manager.set_cost("01", 2.0)
+
+
+def test_get_item(inventory_manager: InventoryManager) -> None:
+    """Test getting item from valid and empty slot."""
+    inventory_manager.add_item("00", "Soda", 10, 1.5)
+    item = inventory_manager.get_item("00")
+    assert item.get_name() == "Soda"
+
+    with pytest.raises(err.EmptySlotError):
+        inventory_manager.get_item("01")
+
+
+# Private Tests
+def test_get_coordinates_from_slotname(inventory_manager: InventoryManager) -> None:
+    """Test private function for getting coordinates for an item for valid and invalid cases."""
+    assert inventory_manager._InventoryManager__get_coordinates_from_slotname("00") == (0, 0)
+
+    with pytest.raises(err.InvalidSlotNameError):
+        inventory_manager._InventoryManager__get_coordinates_from_slotname("A1")
+
+    with pytest.raises(err.InvalidSlotNameError):
+        inventory_manager._InventoryManager__get_coordinates_from_slotname("000")
