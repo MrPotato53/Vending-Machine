b61c8abb44ef43cc0f20676665d6b5c5a4e6bc18
William He <wjhe2@wisc.edu>
2025-03-02 18:57:04 -0600

    abstracted connection pool int db/db_connection.js, abstracted vending machine routes into routes/vending_machine_routes.js, added the following endpoints: view all vending machines, view vending machine by id, create vending machine

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -5,18 +5,12 @@ const mysql = require("mysql2");
 const app = express();
 const port = 5000;
 
-// Database connection
-const db = require("./db_connection");
+// Import routes
+const vendingMachinesRoutes = require("./routes/vending_machine_routes");
 
-// API route to get vending machines
-app.get("/vending-machines", async (req, res) => {
-  try {
-    const [results] = await db.query("SELECT * FROM vending_machines");
-    res.json(results);
-} catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+app.use(express.json());
+
+app.use("/vending-machines", vendingMachinesRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
