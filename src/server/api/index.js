a4628c1567caab22abfa35c00c367ed24ec790cd
William He <wjhe2@wisc.edu>
2025-03-12 14:42:42 -0500

    Added database communicator integration in inventory_manager.py, slight modifications to routes

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Import routes
 const vendingMachinesRoutes = require("./routes/vending_machine");
