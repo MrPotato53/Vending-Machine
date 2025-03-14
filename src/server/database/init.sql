0ac15fef60480002faca11dd8ca5eac201582c99
William He <wjhe2@wisc.edu>
2025-03-13 19:36:34 -0500

    Initial integration of db_signal functions in inventory_manager.py and vending_machine.py

--- src/server/database/init.sql
+++ src/server/database/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS vending_machines (
     vm_row_count INT UNSIGNED NOT NULL,
     vm_column_count INT UNSIGNED NOT NULL,
     vm_mode CHAR(1) NOT NULL -- "i" for idle, "r" for restocking, "t" for transaction
-    -- vm_last_unupdated_operation BIT(1) null means both vendor and customer are up to date, 0 means vendor is not up to date (transaction happened), 1 means customer is not up to date (restocking happened)
+    -- vm_recently_restocked BIT(1) NOT NULL, -- 0 means not recently restocked, 1 means recently restocked
     -- vendor_id INT
 );
 
@@ -22,7 +22,6 @@ CREATE TABLE IF NOT EXISTS inventory_join_table (
     IJT_item_id INT NOT NULL,
     IJT_price DECIMAL(10, 2) UNSIGNED NOT NULL,
     IJT_stock INT UNSIGNED NOT NULL,
-    -- IJT_modified BIT(1) NOT NULL, -- 0 means not modified, 1 means modified
     PRIMARY KEY (IJT_vm_id, IJT_slot_name),
     FOREIGN KEY (IJT_vm_id) REFERENCES vending_machines(vm_id) ON DELETE CASCADE,
     FOREIGN KEY (IJT_item_id) REFERENCES items(item_id) ON DELETE CASCADE
