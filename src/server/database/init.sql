c315a85b76f0345dba274365787225534cd786c5
Daniel Thoman <dkthoman@wisc.edu>
2025-03-19 18:52:59 -0500

    Users & orginizers now in DB

--- src/server/database/init.sql
+++ src/server/database/init.sql
@@ -8,6 +8,12 @@ CREATE TABLE IF NOT EXISTS vending_machines (
     vm_mode CHAR(1) NOT NULL -- "i" for idle, "r" for restocking, "t" for transaction
     -- vm_recently_restocked BIT(1) NOT NULL, -- 0 means not recently restocked, 1 means recently restocked
     -- vendor_id INT
+
+    orgID INT NOT NULL DEFAULT 0, --current implementation only has master users(0)
+    maintainer INT NOT NULL DEFAULT 0, --current implementation only has master users(0)
+
+    FOREIGN KEY (orgID) REFERENCES orgs(ID) ON DELETE CASCADE,
+    FOREIGN KEY (maintainer) REFERENCES users(ID) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS items (
@@ -25,4 +31,27 @@ CREATE TABLE IF NOT EXISTS inventory_join_table (
     PRIMARY KEY (IJT_vm_id, IJT_slot_name),
     FOREIGN KEY (IJT_vm_id) REFERENCES vending_machines(vm_id) ON DELETE CASCADE,
     FOREIGN KEY (IJT_item_id) REFERENCES items(item_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF not EXISTS orgs (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    org_name CHAR(20),
+    org_owner CHAR(20),
+    stripeID char(30) --change to 255 to future proof stripe ID changes
+
+    FOREIGN KEY (org_owner) REFERENCES users(u_name) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+    u_id INT AUTO_INCREMENT PRIMARY KEY,
+    u_name char(20) NOT NULL,
+    email varchar(20,) NOT NULL,
+    
+    u_role CHAR(12) DEFAULT 'maintainer', 
+    orgID INT DEFAULT 0, 
+
+    pasword CHAR(60) NOT NULL, --could hash the position of the encrypt pwd on a table
+
+    FOREIGN KEY (orgID) REFERENCES orgs(ID) on DELETE CASCADE
 );
