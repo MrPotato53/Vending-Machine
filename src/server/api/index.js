cca3a8d366306eb37425ab8ce077c6034d7180b2
Daniel Thoman <dkthoman@wisc.edu>
2025-03-19 22:22:51 -0500

    Added post request for users. Having issue with docker so it is untested

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 // Import routes
 const vendingMachinesRoutes = require("./routes/vending_machine");
 const itemsRoutes = require("./routes/items");
+const userRoutes = require("./routes/users");
 const stripeRoutes = require("./routes/stripe_routes");
 
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/vending-machines", vendingMachinesRoutes);
 app.use("/items", itemsRoutes);
 app.use("/stripes", stripeRoutes);
+app.use("/users", userRoutes);
 
 // Basic healthcheck endpoint for api
 app.get('/health', (req, res) => {
