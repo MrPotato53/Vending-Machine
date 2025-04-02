da721194a43ba88d520636fd32178e8a233734d0
Drew Broch <abroch@wisc.edu>
2025-04-01 23:11:26 -0500

    updates to run_motor to have button trigger motor movement

--- Hardware/run_motor.py
+++ Hardware/run_motor.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from gpiozero import Button
 from hardware_constants import (
     STEP_DELAY,
     STEP_SEQUENCE,
@@ -10,4 +11,27 @@ from stepper_motors import StepperMotor
 
 stepper_motor = StepperMotor(STEPS_PER_QUARTER_REV, STEP_DELAY, STEP_SEQUENCE, STEPPER_1_PINS)
 
-asyncio.run(stepper_motor.rotate_motor(4))
+
+button = Button(23, pull_up=True, bounce_time=0.05)
+
+
+async def monitor_button() -> None:
+    while button.is_pressed and not stepper_motor.moving:
+        t = asyncio.create_task(stepper_motor.rotate_motor(4))
+        await asyncio.sleep(0.2)
+    await asyncio.sleep(0.02)
+
+
+async def print_test_with_while():
+    while True:
+        print("Running something else")
+        await asyncio.sleep(1)
+
+
+async def main():
+    await asyncio.gather(
+        monitor_button(),
+    )
+
+
+asyncio.run(main())
