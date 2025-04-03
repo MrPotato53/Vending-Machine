2167b88bbbdb28d77cb6ed12153a2a47514c4ec3
dkthoman <dkthoman@wisc.edu>
2025-04-02 22:29:41 -0500

    user post route is working

--- src/server/api/db/users.js
+++ src/server/api/db/users.js
@@ -1,18 +1,18 @@
 const db = require("./db_connection");
 const login = require("../email/login");
 
-const userExist = async (userID, res) => {
+const userExist = async (u_name, res) => {
     try{
-        const [results] = await db.query("SELECT * FROM users WHERE u_id = ?", [userID]);
+        const [results] = await db.query("SELECT * FROM users WHERE u_name = ?", [u_name]);
         return results.length > 0;
     }catch(err){
         res.status(500).json({ error: err.message });
     }
 };
 
-const userOTP = async (userID, res) => {
+const userOTP = async (u_name, res) => {
     try{
-        const [results] = await db.query("SELECT email FROM users WHERE u_id = ?", [userID]);
+        const [results] = await db.query("SELECT email FROM users WHERE u_name = ?", [u_name]);
         target = results[0].email;
         otp = await login.email(target);    
         return otp;
