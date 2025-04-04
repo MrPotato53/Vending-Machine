2fbd6add720d595d92202dbc8118309058ed00a3
Drew Broch <abroch@wisc.edu>
2025-04-04 15:59:58 -0500

    Small updates to motor control script

--- Hardware/run_motor.py
+++ Hardware/run_motor.py
@@ -1,7 +1,9 @@
 import asyncio
+from typing import NoReturn
 
 from gpiozero import Button
 from hardware_constants import (
+    BUTTON_1_PIN,
     STEP_DELAY,
     STEP_SEQUENCE,
     STEPPER_1_PINS,
@@ -12,10 +14,10 @@ from stepper_motors import StepperMotor
 stepper_motor = StepperMotor(STEPS_PER_QUARTER_REV, STEP_DELAY, STEP_SEQUENCE, STEPPER_1_PINS)
 
 
-button = Button(23, pull_up=True, bounce_time=0.05)
+button = Button(BUTTON_1_PIN, pull_up=True, bounce_time=0.05)
 
 
-async def monitor_button():
+async def monitor_button() -> NoReturn:
     while True:
         if button.is_pressed:
             if not stepper_motor.moving:
@@ -23,13 +25,14 @@ async def monitor_button():
             await asyncio.sleep(0.2)  # debounce delay
         await asyncio.sleep(0.01)  # polling rate
 
-async def print_background():
+
+async def print_background() -> NoReturn:
     while True:
         print("System idle — waiting for button press... (Testing running other functions)")
         await asyncio.sleep(1)
 
 
-async def main():
+async def main() -> None:
     await asyncio.gather(
         monitor_button(),
         print_background(),
