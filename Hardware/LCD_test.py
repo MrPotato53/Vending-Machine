bd82544893ef5d7fd62a8f1fd905f27591467770
Drew Broch <abroch@wisc.edu>
2025-04-06 16:41:19 -0500

    Added test file for controling LCD with LCDDisplay class

---
+++ Hardware/LCD_test.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from hardware_constants import (
+    I2C_ADDR,
+    LCD_BACKLIGHT,
+    LCD_CHR,
+    LCD_CMD,
+    LCD_E_DELAY,
+    LCD_E_PULSE,
+    LCD_ENABLE,
+    LCD_LINE_1,
+    LCD_LINE_2,
+    LCD_WIDTH,
+)
+from LCD_display import LCDDisplay
+
+
+async def main() -> None:
+    lcd = LCDDisplay(
+        I2C_ADDR,
+        LCD_WIDTH,
+        LCD_LINE_1,
+        LCD_LINE_2,
+        LCD_CHR,
+        LCD_CMD,
+        LCD_BACKLIGHT,
+        LCD_ENABLE,
+        LCD_E_PULSE,
+        LCD_E_DELAY,
+    )
+
+    await lcd.init()
+    await lcd.write("Test short Message", LCD_LINE_1)
+    await lcd.write("This is a test of a very long message", LCD_LINE_2)
+    await asyncio.sleep(5)
+    await lcd.clear()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
