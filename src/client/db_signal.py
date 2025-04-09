b4ace0e5768e9aca0b68a0c60b4ec75a0bd7c75d
William He <wjhe2@wisc.edu>
2025-04-09 15:37:38 -0500

    Moved ping code to its own file, db_ping.py

--- src/client/db_signal.py
+++ src/client/db_signal.py
@@ -1,49 +1,11 @@
-from __future__ import annotations  # noqa: INP001
-
-import time
-from functools import wraps
+from __future__ import annotations
 
 import db_communicator
 import exceptions as err
-import requests
 from api_constants import (
-    BACKEND_HOST,
     NOT_FOUND,
-    SUCCESS,
 )
-
-
-# Repeatedly pings health endpoint of api until valid response is received
-def ping_endpoint():
-    print("Connecting...")
-    while(True):
-        try:
-            response = requests.get(BACKEND_HOST + "/health", timeout=5)
-            if response.status_code == SUCCESS:
-                print("Connection success!")
-                return
-            print("Connection failed: retrying...")
-        except requests.exceptions.RequestException as e:
-            pass
-        time.sleep(5)
-
-
-# Decorator for all endpoint calling functions that tries to call the endpoint
-# If the initial call fails, we ping the api till reconnection, then call it again
-def ping_endpoint_till_connect():
-    def decorator(func):  # noqa: ANN001, ANN202
-        @wraps(func)
-        def wrapper(*args, **kwargs):  # noqa: ANN003, ANN202
-            while True:
-                try:
-                    return func(*args, **kwargs)
-                except (ConnectionError, TimeoutError) as e:  # noqa: PERF203
-                    print("Lost connection. Waiting for API to recover...")
-                    ping_endpoint()
-                    print("Retrying request...")
-                    # Loop will re-call func after API is back
-        return wrapper
-    return decorator
+from db_ping import ping_endpoint_till_connect
 
 
 class VendingMachines:
