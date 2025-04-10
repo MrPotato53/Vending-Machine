06472fccd80075932bb60a074c8acde1ac9d3458
Drew Broch <abroch@wisc.edu>
2025-04-09 23:12:26 -0500

    rearranged where input logic occurs

--- src/client/customer/hardware_manager.py
+++ src/client/customer/hardware_manager.py
@@ -1,4 +1,24 @@
-from customer.Hardware.hardware_constants import *
+from customer.Hardware.hardware_constants import (
+    I2C_ADDR,
+    KEYPAD_COL_PINS,
+    KEYPAD_LAYOUT,
+    KEYPAD_ROW_PINS,
+    LCD_BACKLIGHT,
+    LCD_CHR,
+    LCD_CMD,
+    LCD_E_DELAY,
+    LCD_E_PULSE,
+    LCD_ENABLE,
+    LCD_LINE_1,
+    LCD_LINE_2,
+    LCD_WIDTH,
+    STEP_DELAY,
+    STEP_SEQUENCE,
+    STEPPER_1_PINS,
+    STEPPER_2_PINS,
+    STEPPER_3_PINS,
+    STEPS_PER_QUARTER_REV,
+)
 from customer.Hardware.keypad import AsyncKeypad
 from customer.Hardware.LCD_display import LCDDisplay
 from customer.Hardware.stepper_motors import StepperMotor
@@ -37,21 +57,15 @@ class HardwareManager:
         await self.keypad.start()
 
     async def read_keypad_input(self) -> str:
+        """Reads keyboard input and returns when a special case input has been detected.
+
+        Retrun:
+            str: str of user input.
+        """  # noqa: D401
         self.current_input_string = ""
         while True:
             key = await self.keypad.get_key()
             print(key)
-            self.current_input_string += key
-
-            if DISPENSE_KEY is key:
-                self.current_input_string = self.current_input_string[:-1]
-                return self.current_input_string
-
-            if DELETE_KEY is key:
-                self.current_input_string = self.current_input_string[:-2]
-
-            elif CARD_INFO_KEY is key:
-                return CARD_INFO_KEY
 
     # async def dispense_item(self, slot_name: str):
     #     row, col =
--- src/client/run_vending_machine.py
+++ src/client/run_vending_machine.py
@@ -3,23 +3,38 @@ import asyncio
 from customer.hardware_manager import HardwareManager
 from customer.vending_machine import VendingMachine
 
-vending_machine = VendingMachine(rows=1, columns=3, hardware_id="REALVM", name="Physical_VM")
-
-inv_manager = vending_machine.inv_man
-hardware_manager = HardwareManager(inv_manager)
+from client.customer.Hardware.hardware_constants import CARD_INFO_KEY, DELETE_KEY, DISPENSE_KEY
 
 
-async def main():
-    await hardware_manager.start()
-    try:
-        while True:
-            key = await hardware_manager.read_keypad_input()
-            print(key)
-            # await asyncio.sleep(0.05)
-    finally:
-        if hardware_manager.keypad:
-            await hardware_manager.keypad.close()
+class VendingMachineHardware:
+    async def keypad_moniter(self):
+        await hardware_manager.start()
+        try:
+            while True:
+                key = await hardware_manager.read_keypad_input()
+                print(key)
+                if DISPENSE_KEY is key:
+                    self.current_input_string = self.current_input_string[:-1]
+                    # check that user input is valid
+                    print(self.current_input_string)
+                    self.current_input_string = None
+
+                if DELETE_KEY is key:
+                    self.current_input_string = self.current_input_string[:-2]
+
+                elif CARD_INFO_KEY is key:
+                    pass
+                    # need to use transcation for free function
+        finally:
+            if hardware_manager.keypad:
+                await hardware_manager.keypad.close()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    vending_machine = VendingMachine(rows=1, columns=3, hardware_id="REALVM", name="Physical_VM")
+
+    inv_manager = vending_machine.inv_man
+    hardware_manager = HardwareManager(inv_manager)
+
+    vending_machine = VendingMachineHardware()
+    asyncio.run(vending_machine.keypad_moniter())
