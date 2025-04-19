6ac694cca9b10bc52d899493ce745e13dd1603e2
dkthoman <dkthoman@wisc.edu>
2025-04-18 21:16:06 -0500

    orgs debugged

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 5000;
 const vendingMachinesRoutes = require("./routes/vending_machine");
 const itemsRoutes = require("./routes/items");
 const userRoutes = require("./routes/users");
+const orgRoutes = require("./routes/orgs");
+
 const stripeRoutes = require("./routes/stripe_routes");
+const { request } = require("http");
 
 app.use(express.json());
 
@@ -16,6 +19,7 @@ app.use("/vending-machines", vendingMachinesRoutes);
 app.use("/items", itemsRoutes);
 app.use("/stripes", stripeRoutes);
 app.use("/users", userRoutes);
+app.use("/orgs", orgRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
