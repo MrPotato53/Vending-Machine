1a6fe1462e2ff2b5cb699e15330f784f4946f1ca
William He <wjhe2@wisc.edu>
2025-04-21 10:05:13 -0500

    cors update

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: `http://localhost:8081`,
   origin: `http://localhost`,
+  origin: `http://cs506x19.cs.wisc.edu`,
   methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
