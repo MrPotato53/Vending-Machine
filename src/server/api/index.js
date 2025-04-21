22301e0ef7c456671444e49b1edd83d0ba55dbd2
William He <wjhe2@wisc.edu>
2025-04-21 10:13:13 -0500

    endpoints fix

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 5000;
 
 // enable CORS for your frontend origin (adjust if needed)
 app.use(cors({
-  origin: `http://localhost:8081`,
-  origin: `http://localhost`,
-  origin: `http://cs506x19.cs.wisc.edu`,
+  origin: [
+    'http://localhost:8081',
+    'http://localhost',
+    'http://cs506x19.cs.wisc.edu'
+  ],
   methods: ["GET","POST","PATCH","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true
